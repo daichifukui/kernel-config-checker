1a6963e10d340e66a5f973e1878ee32eec1536d9
Daichi Fukui <daichi.fukui.j@gmail.com>
2020-07-04 13:30:28 +0000

    output expected options if needed
    Signed-off-by: Daichi Fukui <daichi.fukui.j@gmail.com>

--- kcc/cli.py
+++ kcc/cli.py
@@ -5,6 +5,7 @@ SPDX-License-Identifier: GPL-3.0
 
 cli for kernel config checker
 '''
+import re
 import sys
 import argparse
 from typing import Iterator
@@ -13,9 +14,36 @@ from .kconfig import Kconfig
 from .query import KernelSelfProtectionProject
 from .defaults import MUST_BE_SET, MUST_BE_SET_OR_MODULE, MUST_BE_UNSET
 
+def get_expected_opt(opt, msg: str):
+    msg = msg.strip()
+    match = re.search("is not set but is required to be set to y or m", msg)
+    if match:
+        return opt + "=Y \n"  
+    match = re.search("is not set but is required to be set to y", msg)
+    if match:
+        return opt + "=Y \n"  
+    match = re.search("is set but is required to be not set ", msg)
+    if match:
+        return "# " + opt + " is not set \n"  
+    match = re.search("is set as =m but is required to be set to y", msg)
+    if match:
+        return opt + "=Y \n"  
+    match = re.search("is set as =m but is required to be not set", msg)
+    if match:
+        return "# " + opt + " is not set \n"  
+    match = re.search("is not set but is required to be set to m", msg)
+    if match:
+        return opt + "=M \n"  
+    match = re.search("is set  but is required to be set to m", msg)
+    if match:
+        return opt + "=M \n"  
+    return ""
+    
+
 def run(*,
         config_file: str = '',
-        query: bool = False):
+        query: bool = False,
+        expected: bool = False):
     if query:
         must_be_set, must_be_set_or_module, must_be_unset = \
                 KernelSelfProtectionProject()
@@ -31,8 +59,16 @@ def run(*,
     results = kconfig.check(config_file)
     if not results:
         return
+    if expected:
+        f = open('expected_config',mode='w')
     for opt, msg in results.items():
         print(opt, msg)
+        if expected: 
+            expect = get_expected_opt(opt, msg)
+            f.write(expect)
+    if expected:
+        f.close()
+        
     return 1
 
 def cli() -> None:
@@ -40,6 +76,7 @@ def cli() -> None:
     parser.add_argument('config_file', nargs='?', type=argparse.FileType('r'),
             default=sys.stdin)
     parser.add_argument('--query', default=False, action='store_true')
+    parser.add_argument('--expected', default=False, action='store_true')
     args = parser.parse_args()
     ret = run(**vars(args))
     args.config_file.close()
